843703b37408d0caa0be3cc9affdee9985baae93
krupali chauhan <krupali.chauhan@netpairinfotech.com>
2026-02-05 14:31:19 +0530

    Initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Vite + Tailwind Setup ✅
+      </h1>
+      <p className="text-gray-700 mt-4">Tailwind is working perfectly!</p>
+    </div>
+  );
+}
+
+export default App;
